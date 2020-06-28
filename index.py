c7f5cf67cb4cfbe938af36eaaff8e0bc8a5cb568
Anto Tom Abraham <64683098+AntoTomAbraham@users.noreply.github.com>
2020-06-28 21:44:41 +0530

    updated with keys

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ base.title('calculator')
 base.geometry("400x500")
 lbl=Label(base,anchor=SE,)
 lbl.pack(expand=True,fill='both')
+roww=Frame(base)
+roww.pack(expand=True, fill ="both")
+
 row1=Frame(base)
 row1.pack(expand=True, fill ="both")
 
@@ -31,9 +34,14 @@ btn8.pack(side=LEFT,expand= True,fill="both")
 btn9=Button(row1,text="9",borderwidth=0,relief=FLAT,font=("verdana",22))
 btn9.pack(side=LEFT,expand= True,fill="both")
 
-btnx=Button(row1,text="x",borderwidth=0,relief=FLAT,font=("verdana",22))
+btnx=Button(row1,text="x",borderwidth=0,relief=FLAT,font=("verdana",11))
 btnx.pack(side=LEFT,expand= True,fill="both")
 
+btn9=Button(row1,text="sin",borderwidth=0,relief=FLAT,font=("verdana",11))
+btn9.pack(side=LEFT,expand= True,fill="both")
+
+btnx=Button(row1,text="rad",borderwidth=0,relief=FLAT,font=("verdana",11))
+btnx.pack(side=LEFT,expand= True,fill="both")
 
 
 
@@ -46,9 +54,14 @@ btn5.pack(side=LEFT,expand= True,fill="both")
 btn6=Button(row2,text="6",borderwidth=0,relief=FLAT,font=("verdana",22))
 btn6.pack(side=LEFT,expand= True,fill="both")
 
-btni=Button(row2,text="-",borderwidth=0,relief=FLAT,font=("verdana",22))
+btni=Button(row2,text="-",borderwidth=0,relief=FLAT,font=("verdana",11))
 btni.pack(side=LEFT,expand= True,fill="both")
 
+btn6=Button(row2,text="cos",borderwidth=0,relief=FLAT,font=("verdana",11))
+btn6.pack(side=LEFT,expand= True,fill="both")
+
+btni=Button(row2,text="deg",borderwidth=0,relief=FLAT,font=("verdana",11))
+btni.pack(side=LEFT,expand= True,fill="both")
 
 
 
@@ -63,39 +76,54 @@ btn2.pack(side=LEFT,expand= True,fill="both")
 btn3=Button(row3,text="3",borderwidth=0,relief=FLAT,font=("verdana",22))
 btn3.pack(side=LEFT,expand= True,fill="both")
 
-btnp=Button(row3,text="+",borderwidth=0,relief=FLAT,font=("verdana",22))
+btnp=Button(row3,text="+  ",borderwidth=0,relief=FLAT,font=("verdana",11))
 btnp.pack(side=LEFT,expand= True,fill="both")
 
+btn6=Button(row3,text="tan  ",borderwidth=0,relief=FLAT,font=("verdana",11))
+btn6.pack(side=LEFT,expand= True,fill="both")
+
+btni=Button(row3,text="n!",borderwidth=0,relief=FLAT,font=("verdana",11))
+btni.pack(side=LEFT,expand= True,fill="both")
 
 
 
-btnd=Button(row4,text="/",borderwidth=0,relief=FLAT,font=("verdana",22))
+btnd=Button(row4,text="/",borderwidth=0,relief=FLAT,font=("verdana",11))
 btnd.pack(side=LEFT,expand= True,fill="both")
 
 btno=Button(row4,text="0",borderwidth=0,relief=FLAT,font=("verdana",22))
 btno.pack(side=LEFT,expand= True,fill="both")
 
-btndo=Button(row4,text=".",borderwidth=0,relief=FLAT,font=("verdana",22))
+btndo=Button(row4,text=".",borderwidth=0,relief=FLAT,font=("verdana",11))
 btndo.pack(side=LEFT,expand= True,fill="both")
 
-btneq=Button(row4,text="=",borderwidth=0,relief=FLAT,font=("verdana",22))
+btneq=Button(row4,text="  =",borderwidth=0,relief=FLAT,font=("verdana",22))
 btneq.pack(side=LEFT,expand= True,fill="both")
 
+btn6=Button(row4,text="log  ",borderwidth=0,relief=FLAT,font=("verdana",11))
+btn6.pack(side=LEFT,expand= True,fill="both")
+
+btni=Button(row4,text="e",borderwidth=0,relief=FLAT,font=("verdana",11))
+btni.pack(side=LEFT,expand= True,fill="both")
 
 
 
-btn22=Button(row5,text="x^2",borderwidth=0,relief=FLAT,font=("verdana",14))
+btn22=Button(row5,text="x^2",borderwidth=0,relief=FLAT,font=("verdana",11))
 btn22.pack(side=LEFT,expand= True,fill="both")
 
-btn33=Button(row5,text="x^3",borderwidth=0,relief=FLAT,font=("verdana",14))
+btn33=Button(row5,text="x^3",borderwidth=0,relief=FLAT,font=("verdana",11))
 btn33.pack(side=LEFT,expand= True,fill="both")
 
-btnsq=Button(row5,text="C",borderwidth=0,relief=FLAT,font=("verdana",14))
+btnsq=Button(row5,text="C",borderwidth=0,relief=FLAT,font=("verdana",11))
 btnsq.pack(side=LEFT,expand= True,fill="both")
 
-btnpr=Button(row5,text="%",borderwidth=0,relief=FLAT,font=("verdana",14))
+btnpr=Button(row5,text="%",borderwidth=0,relief=FLAT,font=("verdana",11))
 btnpr.pack(side=LEFT,expand= True,fill="both")
 
+btnsq=Button(row5,text="  x^y",borderwidth=0,relief=FLAT,font=("verdana",11))
+btnsq.pack(side=LEFT,expand= True,fill="both")
+
+btnpr=Button(row5,text="mod",borderwidth=0,relief=FLAT,font=("verdana",11))
+btnpr.pack(side=LEFT,expand= True,fill="both")
 
 
 base.mainloop()
