51ac23f4f80ac969541c358fe4e60cb91f70f488
Anto Tom Abraham <64683098+AntoTomAbraham@users.noreply.github.com>
2020-06-30 13:08:37 +0530

    basic calculator created

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from tkinter import *
 base=Tk()
+import math
 val = ""
 operator = ""
 a = 0
@@ -31,6 +32,10 @@ def btn7():
     global val
     val = val+"7"
     data.set(val)
+def flot():
+    global val
+    val = val+"."
+    data.set(val)
 def btn8():
     global val
     val = val+"8"
@@ -42,6 +47,10 @@ def btn9():
 def btn0():
     global val
     val = val+"0"
+    data.set(val)
+def btn00():
+    global val
+    val = val+"00"
     data.set(val)
 def btnplus():
     global a
@@ -83,11 +92,74 @@ def btnc():
     operator = ""
     val = ""
     data.set(val)
+def sin():
+    global val
+    val=float(val)
+    val=math.sin(val)
+    data.set(val)
+def cos():
+    global val
+    val=float(val)
+    val = math.cos(val)
+    data.set(val)
+def log():
+    global val
+    val=int(val)
+    val=math.log(val)
+    data.set(val)
+def tan():
+    global val
+    val=float(val)
+    val = math.tan(val)
+    data.set(val)
+def factorial():
+    global val
+    val = int(val)
+    val = math.factorial(val)
+    data.set(val)
+def rad():
+    global val
+    val=float(val)
+    val = math.radians(val)
+    data.set(val)
+def deg():
+    global val
+    val=int(val)
+    val = math.degrees(val)
+    data.set(val)
+def x2():
+    global val
+    val=int(val)
+    val = val*val*val
+    data.set(val)
+def x3():
+    global val
+    val = int(val)
+    val = val*val*val
+    data.set(val)
+def percentage():
+    global val
+    val=int(val)
+    val = val / 100
+    data.set(val)
+def mod():
+    global a
+    global operator
+    global val
+    a=int(val)
+    operator ="mod"
+    val = val + ".mod"
+    data.set(val)
 def result():
     global a
     global operator
     global val
-    vall=val
+    vall = val
+    if operator == mod:
+        x = int((vall.split("mod")[1]))
+        c = a // x
+        data.set(c)
+        val = str(c)
     if operator == "+":
         x = int((vall.split("+")[1]))
         c=a+x
@@ -114,7 +186,7 @@ rowx=Frame(base,bg="#ffffff")
 rowx.pack(expand=True, fill ="both")
 rowk=Frame(base,bg="#ffffff")
 rowk.pack(expand=True, fill ="both")
-data=StringVar()
+data = StringVar()
 lbl=Label(base,anchor=SE,bg="#c0c0c0",textvariable=data,background = "#ffffff",fg = "#000000",font=22)
 lbl.pack(expand=True,fill='both')
 roww=Frame(base,bg="#c0c0c0")
@@ -137,7 +209,7 @@ btn9=Button(row1,text="9",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana"
 btn9.pack(side=LEFT,expand= True,fill="both")
 btnx=Button(row1,text="x",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",11),command=btnmul)
 btnx.pack(side=LEFT,expand= True,fill="both")
-btn9=Button(row1,text="sin",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",11))
+btn9=Button(row1,text="sin",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",11),command=sin)
 btn9.pack(side=LEFT,expand= True,fill="both")
 btnx=Button(row1,text="=",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",14),command=result)
 btnx.pack(side=LEFT,expand= True,fill="both")
@@ -149,9 +221,9 @@ btn6=Button(row2,text="6",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana"
 btn6.pack(side=LEFT,expand= True,fill="both")
 btni=Button(row2,text="-",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",11),command=btnsub)
 btni.pack(side=LEFT,expand= True,fill="both")
-btn6=Button(row2,text="cos",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",10))
+btn6=Button(row2,text="cos",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",10),command=cos)
 btn6.pack(side=LEFT,expand= True,fill="both")
-btni=Button(row2,text="log",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",10))
+btni=Button(row2,text="log",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",10),command=log)
 btni.pack(side=LEFT,expand= True,fill="both")
 btn1=Button(row3,text="1",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana",22),command=btn1)
 btn1.pack(side=LEFT,expand= True,fill="both")
@@ -161,32 +233,32 @@ btn3=Button(row3,text="3",bg="#c0c0c0",borderwidth=0,relief=FLAT,font=("verdana"
 btn3.pack(side=LEFT,expand= True,fill="both")
 btnp=Button(row3,text="+  ",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=btnplus)
 btnp.pack(side=LEFT,expand= True,fill="both")
-btn6=Button(row3,text="tan  ",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11))
+btn6=Button(row3,text="tan  ",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11),command=tan)
 btn6.pack(side=LEFT,expand= True,fill="both")
-btni=Button(row3,text="n!",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11))
+btni=Button(row3,text="n!",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11),command=factorial)
 btni.pack(side=LEFT,expand= True,fill="both")
-btnd=Button(row4,text="/",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11),command=btndiv)
+btnd=Button(row4,text=" /",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",15),command=btndiv)
 btnd.pack(side=LEFT,expand= True,fill="both")
-btno=Button(row4,text="0",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",22),command=btn0)
+btno=Button(row4,text="  0",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",22),command=btn0)
 btno.pack(side=LEFT,expand= True,fill="both")
-btndo=Button(row4,text=".",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",15))
+btnoo=Button(row4,text=" 00",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",22),command=btn00)
+btnoo.pack(side=LEFT,expand= True,fill="both")
+btndo=Button(row4,text=".",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",15),command=flot)
 btndo.pack(side=LEFT,expand= True,fill="both")
-btneq=Button(row4,text=" rad",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
+btneq=Button(row4,text=" rad",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=rad)
 btneq.pack(side=LEFT,expand= True,fill="both")
-btn6=Button(row4,text="deg  ",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",10))
+btn6=Button(row4,text="deg  ",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",10),command=deg)
 btn6.pack(side=LEFT,expand= True,fill="both")
-btni=Button(row4,text="e",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
-btni.pack(side=LEFT,expand= True,fill="both")
-btn22=Button(row5,text="x2",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
+btn22=Button(row5,text="x2",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=x2)
 btn22.pack(side=LEFT,expand= True,fill="both")
-btn33=Button(row5,text="x3",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
+btn33=Button(row5,text="x3",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=x3)
 btn33.pack(side=LEFT,expand= True,fill="both")
 btnsq=Button(row5,text="C",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=btnc)
 btnsq.pack(side=LEFT,expand= True,fill="both")
-btnpr=Button(row5,text="%",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
+btnpr=Button(row5,text="%",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11),command=percentage)
 btnpr.pack(side=LEFT,expand= True,fill="both")
 btnsq=Button(row5,text="  x^y",borderwidth=0,bg="#c0c0c0",relief=FLAT,font=("verdana",11))
 btnsq.pack(side=LEFT,expand= True,fill="both")
-btnpr=Button(row5,text="mod",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11))
+btnpr=Button(row5,text="mod",borderwidth=0,relief=FLAT,bg="#c0c0c0",font=("verdana",11),command=mod)
 btnpr.pack(side=LEFT,expand= True,fill="both")
 base.mainloop()
